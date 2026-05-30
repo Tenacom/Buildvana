d2f9f68639aca6f1e1d08e9cddcf7c4a01fc5f1e
Riccardo De Agostini <me@riccar.do>
2026-05-30 15:52:06 +0200

    Improve parameter handling in DotNetConfig, as follows:
    - Build `dotnet` paraneter lists declaratively, preferring ternaries to
      `if` blocks
    - Formalize verbosity and output streaming requirements into an
      `InvocationKind` private enum
    - Make the intent of the parameter to `ContinuousIntegrationBuildArg()`
      clearer

---
+++ src/Buildvana.Tool/Services/DotNetService.InvocationKind.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (C) Tenacom and Contributors. Licensed under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+namespace Buildvana.Tool.Services;
+
+partial class DotNetService
+{
+    private enum InvocationKind
+    {
+        /// <summary>
+        /// A normal invocation: `dotnet` accepts the `--verbosity` argument and the user is interested in the output.
+        /// </summary>
+        Normal,
+
+        /// <summary>
+        /// An informational invocation: the user is interested in the output, but `dotnet` does not accept the `--verbosity` argument.
+        /// </summary>
+        // ReSharper disable once UnusedMember.Local
+        Informational,
+
+        /// <summary>
+        /// An internal invocation: the user is not interested in the output, and the `--verbosity` argument, if any, is already set appropriately.
+        /// </summary>
+        Internal,
+    }
+}
