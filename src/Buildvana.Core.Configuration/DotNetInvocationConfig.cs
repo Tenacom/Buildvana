e116b1fd7e212c182bf3449181b57a240c2dcda0
Riccardo De Agostini <me@riccar.do>
2026-05-31 03:01:07 +0200

    Restructure DotNetConfig, adding environment variables to be passed to `dotnet`

---
+++ src/Buildvana.Core.Configuration/DotNetInvocationConfig.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (C) Tenacom and Contributors. Licensed under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Buildvana.Core.Configuration;
+
+public sealed record DotNetInvocationConfig
+{
+    /// <summary>
+    /// Gets extra arguments forwarded to <c>dotnet</c>.
+    /// </summary>
+    [Description("Extra arguments forwarded to `dotnet`.")]
+    public IReadOnlyList<string>? Args { get; init; }
+
+    /// <summary>
+    /// Gets environment variables forwarded to <c>dotnet</c>, keyed by variable name.
+    /// </summary>
+    [Description("Environment variables forwarded to `dotnet`, keyed by variable name.")]
+    public IReadOnlyDictionary<string, string>? Env { get; init; }
+}
